bb564adbc76cdd4f02e7ec3d9e8d71f339720423
Laby Damaro Camara <ldamaro98@gmail.com>
2021-01-30 20:57:56 +0100

    Configuration de bootstrap et reactstrap

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NabarBrand, NavbarBrand } from 'reactstrap';
 
-const element = (<h1>Hello, World !</h1>)
 
-const element2 = React.createElement(
-  'h1',
-  null,
-  'Hello, World !!!!'
-);
-
-const personnes = [ 
-  {
-    nom: 'Diallo',
-    prenom: 'Mouctar',
-    role: 'Etudaint'
-  },
-  {
-    nom: 'Camara',
-    prenom: 'Laby Damaro',
-    role: 'Développeur'
-  },
-  {
-    nom: 'Diallo',
-    prenom: 'Mouctar',
-    role: 'Etudaint'
-  },
-    {
-      nom: 'Diallo',
-      prenom: 'Mouctar',
-      role: 'Etudaint'
-    }
-  
-];
-
-const [p, ...autres] = personnes;
-let nom="Laby Damaro", prenom="Camara";
-[nom, prenom] = [prenom, nom];
-
-const styles = {color:'green', fontStyle: 'bold', textDecoration: 'line-through'};
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{...styles, fontSize: '40px'}}>
-        <span>Nom: {p.nom}</span><br />
-        <span>Prénom: {p.prenom}</span><br />
+      <div className="App">
+        <Navbar dark color="primary">
+            <div className="container">
+              <div className="row">
+                  <NavbarBrand href="/">Bible Of Data Science</NavbarBrand>
+              </div>
+            </div>
+        </Navbar>
       </div>
     );
   }
