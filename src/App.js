cf8917b8f4485d9ebd5172deadfbd5ede4c73475
Laby Damaro Camara <ldamaro98@gmail.com>
2021-01-30 20:51:21 +0100

    introduction a JSX

--- src/App.js
+++ src/App.js
@@ -2,24 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const element = (<h1>Hello, World !</h1>)
+
+const element2 = React.createElement(
+  'h1',
+  null,
+  'Hello, World !!!!'
+);
+
+const personnes = [ 
+  {
+    nom: 'Diallo',
+    prenom: 'Mouctar',
+    role: 'Etudaint'
+  },
+  {
+    nom: 'Camara',
+    prenom: 'Laby Damaro',
+    role: 'Développeur'
+  },
+  {
+    nom: 'Diallo',
+    prenom: 'Mouctar',
+    role: 'Etudaint'
+  },
+    {
+      nom: 'Diallo',
+      prenom: 'Mouctar',
+      role: 'Etudaint'
+    }
+  
+];
+
+const [p, ...autres] = personnes;
+let nom="Laby Damaro", prenom="Camara";
+[nom, prenom] = [prenom, nom];
+
+const styles = {color:'green', fontStyle: 'bold', textDecoration: 'line-through'};
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{...styles, fontSize: '40px'}}>
+        <span>Nom: {p.nom}</span><br />
+        <span>Prénom: {p.prenom}</span><br />
       </div>
     );
   }
